aca272f796103ec7d228081058c1080dd7fa5480
VBO <rediss@yandex.ru>
2017-07-07 16:53:22 +1000

    img min

--- gulpfile.js
+++ gulpfile.js
@@ -2,8 +2,9 @@ var gulp = require('gulp'),
 		browserSync = require('browser-sync'),
 		autoprefixer = require('gulp-autoprefixer'),
 		pug = require('gulp-pug'),
-		cssnano = require('gulp-cssnano');
- 
+		cssnano = require('gulp-cssnano'),
+		imagemin = require('gulp-imagemin');
+
 gulp.task('pug', function buildHTML() {
 	return gulp.src('app/templates/*.pug')
 		.pipe(pug({
@@ -17,17 +18,27 @@ gulp.task('css', () =>
 		.pipe(gulp.dest('dist/css'))
 );
 
+gulp.task('img-min', () =>
+	gulp.src('app/img/*')
+		.pipe(imagemin([
+			imagemin.gifsicle({ interlaced: true }),
+			imagemin.jpegtran({ progressive: true }),
+			imagemin.optipng({ optimizationLevel: 15 }),
+			imagemin.svgo({ plugins: [{ removeViewBox: true }] })
+		]))
+		.pipe(gulp.dest('dist/img'))
+);
+
 gulp.task('browser-sync', function () {
 	browserSync({
 		server: {
 			baseDir: 'dist'
 		},
-		 port: 8080,
-    open: true,
+		port: 8080,
+		open: true,
 		notify: true
 	});
 });
-
 
 gulp.task('build-css', () =>
 	gulp.src('app/css/*.css')
@@ -38,7 +49,6 @@ gulp.task('build-css', () =>
 		.pipe(cssnano())
 		.pipe(gulp.dest('dist/css'))
 );
-
 
 gulp.task('watch', ['pug','css', 'browser-sync'], function () {
 	gulp.watch('app/css/**/*.css', ['css']);

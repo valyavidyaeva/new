8e0450613d8edbb0d9d2ad0ae129ee4db5ea7575
valyavidyaeva <valyavidyaeva@mail.ru>
2017-08-18 11:02:42 +1000

    menu script, index page

---
+++ dist/js/main.js
@@ -0,0 +1,20 @@
+var   menuToggle = document.getElementById('menu-toggle'),
+      innerToggle = document.getElementById('toggle-inner'),
+      mainNav = document.getElementById('nav'),
+      innerNav = document.getElementById('inner-nav');
+
+function toggle(el) {
+  el.previousElementSibling.classList.toggle('toggled');
+  el.classList.toggle('open');
+  window.onresize = function(){ checkWindow();};
+};
+function checkWindow(){
+  if (window.innerWidth >= 769) {
+    mainNav.classList.remove('open');
+    innerNav.classList.remove('open');
+    menuToggle.classList.remove('toggled');
+    innerToggle.classList.remove('toggled');
+  }
+}
+innerToggle.onclick = function(){toggle(innerNav);};
+menuToggle.onclick = function(){toggle(mainNav);};
--- gulpfile.js
+++ gulpfile.js
@@ -6,10 +6,12 @@ var gulp = require('gulp'),
 		imagemin = require('gulp-imagemin'),
 		sass = require('gulp-sass'),
 		plumber = require('gulp-plumber'),
+		errorHandler = require('gulp-plumber-error-handler'),
 		watch = require('gulp-watch');
 
 gulp.task('pug', function buildHTML() {
 	return gulp.src('app/templates/pages/*.pug')
+		.pipe(plumber({errorHandler: errorHandler(`Error in \'templates\' task`)}))
 		.pipe(pug({
 			pretty: true
 		}))
@@ -31,7 +33,7 @@ gulp.task('css', () =>
 );
 
 gulp.task('img-min', () =>
-	gulp.src('app/img/*')
+	gulp.src('app/img/**/*')
 		.pipe(imagemin([
 			imagemin.gifsicle({ interlaced: true }),
 			imagemin.jpegtran({ progressive: true }),
@@ -62,9 +64,16 @@ gulp.task('build-css', () =>
 		.pipe(gulp.dest('dist/css'))
 );
 
+gulp.task('js', function() {  
+	return gulp.src('app/js/*.js')
+			.pipe(gulp.dest('dist/js'))
+});
+
 gulp.task('default', ['browser-sync'], function () {
 	gulp.watch('app/sass/**/*.sass', ['sass']);
 	gulp.watch('app/templates/pages/*.pug', ['pug']);
+	gulp.watch('app/js/*.js', ['js']);
 	gulp.watch('dist/css/**/*.css', browserSync.reload);
 	gulp.watch('dist/*.html', browserSync.reload);
+	gulp.watch('dist/js/*.js', browserSync.reload);
 });
